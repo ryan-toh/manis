e0ebbda7f97333e57687a122d02e840b19a48103
ryan-toh <ryan_toh@outlook.sg>
2025-08-31 03:46:25 +0800

    Why does this not work?

--- ui.py
+++ ui.py
@@ -31,12 +31,12 @@ class RedactorGUI(tk.Tk):
         root.pack(fill="both", expand=True)
 
         # Logo
-        img = Image.open("assets/logo.png")
-        img = img.resize((685, 100))
-        img = ImageTk.PhotoImage(img)
-        logo_row = tk.Label(root, image = img)
-        logo_row.image = img
-        logo_row.pack(side = "top", fill = "both", expand = "no", pady=(0,10))
+        # img = Image.open("assets/logo.png")
+        # img = img.resize((685, 100))
+        # img = ImageTk.PhotoImage(img)
+        # logo_row = tk.Label(root, image = img)
+        # logo_row.image = img
+        # logo_row.pack(side = "top", fill = "both", expand = "no", pady=(0,10))
 
         # Input
         in_row = ttk.Frame(root)
